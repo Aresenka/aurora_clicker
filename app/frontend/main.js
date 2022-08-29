74a455d62c22b05cfcb86c054f3111ba9cd5bd06
Xyzjesus <konstprakhov@gmail.com>
2022-08-29 14:36:02 +0000

    add metamask

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -1,14 +1,20 @@
 import './style.css';
 import { setupCounter } from './counter.js';
+import MetaMaskOnboarding from '@metamask/onboarding';
+
+const onboarding = new MetaMaskOnboarding();
+onboarding.startOnboarding();
+
 
 document.querySelector('#app').innerHTML = `
   <div>
-    </a>
-    <h1>Hello World!</h1>
+    <h1>Aurora Clicker</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
   </div>
 `;
+
+
 
 setupCounter(document.querySelector('#counter'));
